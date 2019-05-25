1f3c264ef4cdf92b70b5d251d5fe22975e5423a4
Nikos <nik.ntf1@gmail.com>
2019-05-26 02:49:29 +0300

    ember octane init app

---
+++ README.md
@@ -0,0 +1,24 @@
+## TESTING AND LEARNING EMBER OCTANE
+ Developing an application for posting editing and filter news with ember octane
+
+## Prerequisites
+
+You will need the following things properly installed on your computer.
+
+* [Git](https://git-scm.com/)
+* [Node.js](https://nodejs.org/) (with npm)
+* [Ember CLI](https://ember-cli.com/)
+* [Google Chrome](https://google.com/chrome/)
+
+## Installation
+
+* `git clone <repository-url>` this repository
+* `cd octane-rd-new`
+* `npm install`
+
+## Run / Dev
+
+* `ember serve`
+* Visit your app at [http://localhost:4200](http://localhost:4200).
+* Visit your tests at [http://localhost:4200/tests](http://localhost:4200/tests).
+
---
+++ app/controllers/application.js
@@ -0,0 +1,65 @@
+import Controller from '@ember/controller';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
+
+export default class ApplicationController extends Controller {
+  @tracked count = 0;
+  @tracked stringName;
+  // INPUT TESTS TACKED PROPERTIES
+  @tracked title;
+  @tracked date;
+  @tracked version;
+
+  /**
+   *    ACTIONS
+   */
+  @action increment() {
+    this.count++;
+  }
+
+  @action decrement() {
+    this.count--;
+  }
+  @action getDate(){
+    return this.date;
+  }
+
+  @action message(){
+    let m = prompt('Enter something!!');
+    return this.stringName = m;
+  }
+  @action handleDate(e){
+    this.date = e.target.value;
+    console.log(`Date: ${this.date}`);
+  }
+
+  @action handleTitle(e){
+    this.title = e.target.value;
+    console.log(`Title: ${this.title}`);
+  }
+
+  @action handleVersion(e){
+    this.version = e.target.value;
+    console.log(`Version: ${this.version}`);
+  }
+
+  @action submitForm(e){
+    e.preventDefault();
+    console.log(`State Date: ${this.date},${this.title},${this.version}`)
+  }
+// Static array of objects for demo purposes
+  itRdNews=[
+    {
+      id:1,
+      date: '12/05/19',
+      title:'test',
+      version:'Fixes on unit tests v0.1.0'
+    },
+    {
+      id:2,
+      date: '12/07/18',
+      title:'Ui',
+      version:'Ui bugs fixed v0.2.0'
+    }
+  ]
+}
