cc6623806fc832d7cce7d479233a6251dba5af28
Nikos <nik.ntf1@gmail.com>
2019-05-26 03:34:38 +0300

    create rd news component .js with 2 actions

--- app/components/create-rd-news.js
+++ app/components/create-rd-news.js
@@ -1,4 +1,13 @@
 import Component from '@glimmer/component';
+import { action } from '@ember/object';
 
 export default class CreateRdNewsComponent extends Component {
+  // Action for displaying the modal
+  @action createModal(){
+    return document.querySelector('.modal').style.display='block';
+  }
+  // Action for closing the modal
+  @action closeModal(){
+    return document.querySelector('.modal').style.display='none';
+  }
 }
