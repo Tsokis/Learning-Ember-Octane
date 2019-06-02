3dc728aa513035d21be793c098a6ab92ec861e1b
Nikos <nik.ntf1@gmail.com>
2019-06-02 20:18:13 +0300

    title and version filter

--- app/components/filter-data.js
+++ app/components/filter-data.js
@@ -5,11 +5,23 @@ import { tracked } from '@glimmer/tracking';
 export default class FilterDataComponent extends Component {
   // filter-date
   @tracked fdate;
+  @tracked ftitle;
+  @tracked fversion;
 
   get dateByClass(){
     let date = document.querySelectorAll('.date');
     return date;
   }
+  get titleByClass(){
+    let title = document.querySelectorAll('.title');
+    return title;
+  }
+
+  get versionByClass(){
+    let version = document.querySelectorAll('.version');
+    return version;
+  }
+
 
   //Filter table date  by date
   @action filterInputDate(props,e) {
@@ -37,9 +49,53 @@ export default class FilterDataComponent extends Component {
 
 
   //Filter table title by title
+  @action filterInputTitle(props,e) {
+    this.ftitle = e.target.value;
+    props.filter(el => {
+      if(el.title === this.ftitle){
+        let arr = [...this.titleByClass];
+        arr.filter(val => {
+          if(val.innerHTML.includes(this.ftitle)) {
+            let mainbody = document.querySelector('.mainbody');
+            val.parentElement.classList.add('selected');
+            let e = mainbody.childNodes;
+            let eArr = [...e];
+            this.ftitle = '';
+            eArr.filter(el => {
+              if(el.className === 'trbody'){
+                el.className = 'trbody hide'
+              }
+            });
+          }
+        })
+      }
+    })
+  }
 
   //Filter table version by version
-
+  // TODO FILTER VERSION BY MATCHING STRING AND NOT BY FULL VERSION NAME
+  @action filterInputVersion(props,e) {
+    this.fversion = e.target.value;
+    props.filter(el => {
+      if(el.version === this.fversion){
+        let arr = [...this.versionByClass];
+        arr.filter(val => {
+          if(val.innerHTML.includes(this.fversion)) {
+            let mainbody = document.querySelector('.mainbody');
+            val.parentElement.classList.add('selected');
+            let e = mainbody.childNodes;
+            let eArr = [...e];
+            this.fversion = '';
+            eArr.filter(el => {
+              if(el.className === 'trbody'){
+                el.className = 'trbody hide'
+              }
+            });
+          }
+        })
+      }
+    })
+  }
 
 
 }
