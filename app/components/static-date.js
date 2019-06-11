9499e64bd88ad814f60c467d7892b3412d552b25
Nikos <nik.ntf1@gmail.com>
2019-06-11 23:06:21 +0300

    dummy calendar display

---
+++ app/components/static-date.js
@@ -0,0 +1,8 @@
+import Component from '@glimmer/component';
+
+export default class StaticDateComponent extends Component {
+  tr = 6
+  // pianoume tin hmera pou eimaste px 4 h 5 h 20 ktl..
+  // prin apo autes petame classeis me black tr
+  //kanoume diplay tis meres meta apo thn hmera pou eimaste px an eimaiste ston 4h tote 5,6,7 ktl..
+}
